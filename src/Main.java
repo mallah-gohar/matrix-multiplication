da93b4591d5764cc72719e3c09626d9380b92154
iqtadanabi <iqtadanabisoomro@gmail.com>
2024-05-18 19:55:34 +0500

    Declared variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // Method to multiply two matrices
+    public static int[][] multiplyMatrices(int[][] firstMatrix, int[][] secondMatrix) {
+        int rowsFirst = firstMatrix.length;
+        int columnsFirst = firstMatrix[0].length;
+        int columnsSecond = secondMatrix[0].length;
     }
 }
